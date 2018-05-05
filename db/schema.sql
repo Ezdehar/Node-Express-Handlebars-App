188fe21f6d1542b49ec53433aeabde44fd908dca
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-05-05 03:34:37 -0400

    update

--- db/schema.sql
+++ db/schema.sql
@@ -10,10 +10,4 @@ CREATE TABLE burgers(
   primary key(item_id)
 );
 
-INSERT INTO burgers (item_id, burger_name, department_name, devoured)
-VALUES 
-("1", "Cheese Burger", "McDonalds", 0),
-("2", "Veggie Burger", "Whole Foods", 0),
-("3", "Whopper", "Burger King", 0);
-
 
