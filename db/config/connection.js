b6bd796c1446e7435579e3611f8abdace4714d19
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-05-05 08:57:23 -0400

    update

--- db/config/connection.js
+++ db/config/connection.js
@@ -4,15 +4,19 @@
 
 // Require mysql
 var mysql = require("mysql");
-
+var connection;
 // Set up our connection information
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Cardinal1",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "Cardinal1",
+    database: "burgers_db"
+  });
+};
 
 // Connect to the database
 connection.connect(function(err) {
