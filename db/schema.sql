92282568b6e3a51ff1da9940fb80becf229fd6e9
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-05-05 03:29:31 -0400

    update

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+CREATE TABLE burgers(
+  item_id INT AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(45) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT 0,
+  primary key(item_id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (item_id, burger_name, department_name, devoured)
+VALUES 
+("1", "Cheese Burger", "McDonalds", 0),
+("2", "Veggie Burger", "Whole Foods", 0),
+("3", "Whopper", "Burger King", 0);
