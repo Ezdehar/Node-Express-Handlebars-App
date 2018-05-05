da917077a809002dee7d8a9e5388741c8538443d
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-05-05 04:53:55 -0400

    update

--- db/schema.sql
+++ db/schema.sql
@@ -9,15 +9,3 @@ CREATE TABLE burgers(
   devoured BOOLEAN DEFAULT false,
   primary key(item_id)
 );
-
-
-DROP DATABASE IF EXISTS;
-
-USE burgers_db;
-CREATE TABLE burgers(
-  item_id INT AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(45) NOT NULL,
-  department_name VARCHAR(45) NOT NULL,
-  devoured BOOLEAN DEFAULT false,
-  primary key(item_id)
-);
